9132c99439265a411a4cfeeca600f73b85f57e16
Yuki <kt11273@gmail.com>
2015-02-28 02:53:10 +0800

    def userAdd

--- futureMessage/futureMessage/urls.py
+++ futureMessage/futureMessage/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/',views.checkLogin),
     url(r'^page/login/',views.loginPage),
+	url(r'^page/userAdd',view.userAdd),
+	
 )
--- futureMessage/message/views.py
+++ futureMessage/message/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from django.db import connection
+import random
 # git test
 # Create your views here.
 @csrf_exempt
@@ -18,11 +19,21 @@ def checkLogin(request):
     password = request.POST["password"]
     success = User.;objects.filter(Q(userEmail=email)&Q(userPassword=password)).count()
     if success == 0:
-	
+		return render_to_response('LoginFail.html')
     else
-	
+		return render_to_response('LoginSuccess.html')
     return HttpResponse(data)
+	
 
 def loginPage(request):
     return render_to_response('Login.html')
-    
+  
+def userAdd(request):
+	email = request.POST["email"]
+	password = request.POST["password"]
+	checkCode = random.randint(1000,10000)
+	
+	user = User(userEmail=email,userPassword=password,checkCode=checkCode)
+	user.save()
+	
+	return render_to_response('userAdd.html')
