918a7623e11d75479b8346ba6155813d50126065
andy <andy.lin@cellopoint.com>
2015-02-27 23:51:49 +0800

    test login

--- futureMessage/message/views.py
+++ futureMessage/message/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from models import User
+from models import Message
+from models import Send
 # Create your views here.
+def checkLogin(request):
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    User.objects.filter(Q(userEmail=email)&Q(userPassword=password)).count()
+    return HttpResponse(data)
