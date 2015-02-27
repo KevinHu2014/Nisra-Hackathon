350707879e4f1f5410d261a766b6b3a909b5ca85
andy <andy.lin@cellopoint.com>
2015-02-28 00:16:44 +0800

    add html

--- futureMessage/message/views.py
+++ futureMessage/message/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-<<<<<<< HEAD
+
 from models import User
 from models import Message
 from models import Send
-=======
+
 # git test
->>>>>>> 6af46303fc0b315c87989838b0f5159ffe9255fe
 # Create your views here.
 def checkLogin(request):
     email = request.POST["email"]
     password = request.POST["password"]
-
-    User.objects.filter(Q(userEmail=email)&Q(userPassword=password)).count()
+    success = User.objects.filter(Q(userEmail=email)&Q(userPassword=password)).count()
+    if success == 0:
+        data = 'Login Error'
+    else:
+        data = 'Login Success'
     return HttpResponse(data)
